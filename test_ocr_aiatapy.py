bd7fb46ab1de99c863c9b043bd4b4322e33f40ce
phawitb <96680592+phawitb@users.noreply.github.com>
2024-02-07 23:11:56 +0700

    Update test_ocr_aiatapy.py

--- test_ocr_aiatapy.py
+++ test_ocr_aiatapy.py
@@ -29,109 +29,112 @@ import pytesseract
 import time
 import streamlit as st
 
-def aiocr(reader,img_path,iscorrection):
-    def is_base64_image(encoded_text):
-        try:
-            decoded_data = base64.b64decode(encoded_text)
-            if isinstance(decoded_data, bytes):
-                return True
-            else:
-                return False
-        except Exception as e:
-            return False
+
+
+
+# def aiocr(reader,img_path,iscorrection):
+#     def is_base64_image(encoded_text):
+#         try:
+#             decoded_data = base64.b64decode(encoded_text)
+#             if isinstance(decoded_data, bytes):
+#                 return True
+#             else:
+#                 return False
+#         except Exception as e:
+#             return False
     
-    def save_base64_to_jpg(base64_data):
-        try:
-            decoded_data = base64.b64decode(base64_data)
-            image_stream = BytesIO(decoded_data)
-            image = Image.open(image_stream)
-            image.save('current_img.jpg', format='JPEG')
-        except Exception as e:
-            print(f"Error: {e}")
+#     def save_base64_to_jpg(base64_data):
+#         try:
+#             decoded_data = base64.b64decode(base64_data)
+#             image_stream = BytesIO(decoded_data)
+#             image = Image.open(image_stream)
+#             image.save('current_img.jpg', format='JPEG')
+#         except Exception as e:
+#             print(f"Error: {e}")
         
-    def loadimgfromurl(url):
-        # url = 'https://images.squarespace-cdn.com/content/v1/54981918e4b0bff4592d6daa/1489005122989-X3H9A1RGI08CPC1QFCST/fee.jpg?format=2500w'
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content))
-        img.save("current_img.jpg")
+#     def loadimgfromurl(url):
+#         # url = 'https://images.squarespace-cdn.com/content/v1/54981918e4b0bff4592d6daa/1489005122989-X3H9A1RGI08CPC1QFCST/fee.jpg?format=2500w'
+#         response = requests.get(url)
+#         img = Image.open(BytesIO(response.content))
+#         img.save("current_img.jpg")
     
-    def is_valid_url(url):
-        try:
-            result = urlparse(url)
-            return all([result.scheme, result.netloc])
-        except ValueError:
-            return False
+#     def is_valid_url(url):
+#         try:
+#             result = urlparse(url)
+#             return all([result.scheme, result.netloc])
+#         except ValueError:
+#             return False
         
-    def ocr_tesseract(img_path):
-        # img_path = 'imgs/191984.jpg'
-        im = Image.open(img_path)
+#     def ocr_tesseract(img_path):
+#         # img_path = 'imgs/191984.jpg'
+#         im = Image.open(img_path)
         
-        im = im.convert("L") #แปลงให้เป็นภาพขาวดำ
-        # pytesseract.image_to_string(im, lang='tha+eng',preserve_interword_spaces=1)
-        custom_config = r'-l tha+eng --dpi 300 --oem 3 --psm 4'  # OCR Engine Mode 3, Page Segmentation Mode 6
-        text = pytesseract.image_to_string(im, config=custom_config)
-        return text
+#         im = im.convert("L") #แปลงให้เป็นภาพขาวดำ
+#         # pytesseract.image_to_string(im, lang='tha+eng',preserve_interword_spaces=1)
+#         custom_config = r'-l tha+eng --dpi 300 --oem 3 --psm 4'  # OCR Engine Mode 3, Page Segmentation Mode 6
+#         text = pytesseract.image_to_string(im, config=custom_config)
+#         return text
         
-    def ocr_easyocr(reader,path):
-        result = reader.readtext(path,detail = 0)
-        return '\n'.join(result)
+#     def ocr_easyocr(reader,path):
+#         result = reader.readtext(path,detail = 0)
+#         return '\n'.join(result)
         
-    def correction(result):
-        result = result.split('\n')
-        R = ''
-        for r in result:
-            w = word_tokenize(r, engine="newmm")
-            # print(w)
-            S = []
-            for ww in w:
-                # print(ww)
-                if len(ww) > 2:
-                    s = spell(ww,  engine="pn")
-                    S.append(s[0])
-                else:
-                    S.append(ww)
-            R += ''.join(S) + '\n'
-            # print(''.join(S))
-        return R
+#     def correction(result):
+#         result = result.split('\n')
+#         R = ''
+#         for r in result:
+#             w = word_tokenize(r, engine="newmm")
+#             # print(w)
+#             S = []
+#             for ww in w:
+#                 # print(ww)
+#                 if len(ww) > 2:
+#                     s = spell(ww,  engine="pn")
+#                     S.append(s[0])
+#                 else:
+#                     S.append(ww)
+#             R += ''.join(S) + '\n'
+#             # print(''.join(S))
+#         return R
     
-    start = time.time()
+#     start = time.time()
 
-    #check is url
-    if is_valid_url(img_path):
-        print('is url')
-        loadimgfromurl(img_path)
-        img_path = 'current_img.jpg'
-    #check is base64
-    elif is_base64_image(img_path):
-        print('is base64')
-        save_base64_to_jpg(img_path)
-        img_path = 'current_img.jpg'
+#     #check is url
+#     if is_valid_url(img_path):
+#         print('is url')
+#         loadimgfromurl(img_path)
+#         img_path = 'current_img.jpg'
+#     #check is base64
+#     elif is_base64_image(img_path):
+#         print('is base64')
+#         save_base64_to_jpg(img_path)
+#         img_path = 'current_img.jpg'
         
-    #check text in image by tesseract
-    text_tesseract = ocr_tesseract(img_path)
+#     #check text in image by tesseract
+#     text_tesseract = ocr_tesseract(img_path)
 
-    tesseract_time = time.time()
+#     tesseract_time = time.time()
     
-    #easyocr
-    text_easyocr = None
-    if text_tesseract:
-        text_easyocr = ocr_easyocr(reader,img_path)
-        # print(text_easyocr)
+#     #easyocr
+#     text_easyocr = None
+#     if text_tesseract:
+#         text_easyocr = ocr_easyocr(reader,img_path)
+#         # print(text_easyocr)
 
-    easyocr_time = time.time()
+#     easyocr_time = time.time()
 
-    #correction
-    if iscorrection and text_easyocr:
-        text_easyocr = correction(text_easyocr)
+#     #correction
+#     if iscorrection and text_easyocr:
+#         text_easyocr = correction(text_easyocr)
 
-    correction_time = time.time()
+#     correction_time = time.time()
 
-    print('tesseract_time',tesseract_time - start)
-    print('easyocr_time',easyocr_time - tesseract_time)
-    print('correction_time',correction_time - easyocr_time)
-    print('Total time',time.time()-start)
+#     print('tesseract_time',tesseract_time - start)
+#     print('easyocr_time',easyocr_time - tesseract_time)
+#     print('correction_time',correction_time - easyocr_time)
+#     print('Total time',time.time()-start)
         
-    return text_easyocr
+#     return text_easyocr
 
 with st.spinner('Load model...'):
     if 'reader' not in st.session_state:
@@ -149,7 +152,9 @@ if img_input:
 
     strat_time = time.time()
     with st.spinner('Wait for it...'):
-        text_ocr = aiocr(reader,img_input,iscorrection)
+        # text_ocr = aiocr(reader,img_input,iscorrection)
+        result = reader.readtext(img_input,detail = 0)
+        text_ocr = '\n'.join(result)
 
     text_ocr = f'[{round(time.time()-strat_time,2)}sec]{text_ocr}'
 
@@ -160,13 +165,16 @@ if img_input:
         st.write(text_ocr)
 
 else:
-    uploaded_file = st.file_uploader("Choose a Images file", accept_multiple_files=False,type=['png','jpeg','jpg'])
-    if uploaded_file:
+    uploaded_files = st.file_uploader("Choose a Images file", accept_multiple_files=True,type=['png','jpeg','jpg'])
+    for uploaded_file in uploaded_files:
+        # if uploaded_file:
         bytes_data = uploaded_file.read()
 
         strat_time = time.time()
         with st.spinner('OCR..Wait for it...'):
-            text_ocr = aiocr(reader,bytes_data,iscorrection)
+            # text_ocr = aiocr(reader,bytes_data,iscorrection)
+            result = reader.readtext(bytes_data,detail = 0)
+            text_ocr = '\n'.join(result)
 
         text_ocr = f'[{round(time.time()-strat_time,2)}sec]{text_ocr}'
         
